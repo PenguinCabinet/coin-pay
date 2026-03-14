609f6b4326c2237416b0fcb328458e497f71b8e9
PenguinCabinet <kage2.kage22@gmail.com>
2026-03-14 14:11:15 +0900

    誤ったjsonをPOSTした際のエラーハンドリングを追加

--- register.php
+++ register.php
@@ -3,8 +3,13 @@
 /*データベースを読み込み*/
 $db = new PDO("sqlite:database.db");
 
-/*php://inputは、HTTPリクエストのBody*/
-$data = json_decode(file_get_contents("php://input"), true);
+try{
+    /*php://inputは、HTTPリクエストのBody*/
+    $data = json_decode(file_get_contents("php://input"), true, 512, JSON_THROW_ON_ERROR);
+}catch (JsonException $e) {
+    http_response_code(400);
+    exit;
+}
 
 $username = $data["username"];
 $password = password_hash($data["password"], PASSWORD_DEFAULT);
