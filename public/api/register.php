38e66e468679a1a5e074a103b81575201712e5aa
PenguinCabinet <kage2.kage22@gmail.com>
2026-03-15 23:42:10 +0900

    api/register.phpのエラーハンドリングとバリデーションの実装 close #1

--- public/api/register.php
+++ public/api/register.php
@@ -3,19 +3,50 @@ require "../../common/login.php";
 
 $db = new PDO("sqlite:../../database/database.db");
 
+/*POSTされたデータのバリデーション*/
 if (!(isset($_POST["username"])&&isset($_POST["password"]))) {
     http_response_code(400);
     exit;
 }
+if ($_POST["username"]==""||$_POST["password"]=="") {
+    http_response_code(400);
+    exit;
+}
+
 $username = $_POST["username"];
 $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
+$stmt = $db->prepare("SELECT * FROM users WHERE username = ?");
+$stmt->execute([$username]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+/*ユーザーが存在する場合、終了 */
+if ($user !== false) {
+    http_response_code(400);
+    exit;
+}
+
+$db->beginTransaction();
+
 $stmt = $db->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
-$stmt->execute([$username, $password]);
+$result = $stmt->execute([$username, $password]);
+/*SQL文が実行に失敗した場合、ロールバック */
+if (!$result) {
+    $db->rollBack();
+    http_response_code(500);
+    exit;
+}
 $user_id = $db->lastInsertId();
 
 $stmt = $db->prepare("INSERT INTO users_data (user_id, owncoin) VALUES (?, ?)");
-$stmt->execute([$user_id, 50]);
+$result = $stmt->execute([$user_id, 50]);
+/*SQL文が実行に失敗した場合、ロールバック */
+if (!$result) {
+    $db->rollBack();
+    http_response_code(500);
+    exit;
+}
+
+$db->commit();
 
 http_response_code(201);
 
