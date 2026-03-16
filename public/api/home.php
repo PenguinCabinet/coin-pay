a888db53ab6c24e7f6e55106a070b5e30e43a774
PenguinCabinet <kage2.kage22@gmail.com>
2026-03-16 19:17:44 +0900

    api/home.phpのエラーハンドリングとバリデーションの実装、リファクタリング

--- public/api/home.php
+++ public/api/home.php
@@ -5,16 +5,26 @@ require "../../common/auth.php";
 $db = new PDO("sqlite:../../database/database.db");
 $userId = authenticate($db);
 
-$stmt1 = $db->prepare("SELECT * FROM users WHERE id = ?");
-$stmt1->execute([$userId]);
-$row1 = $stmt1->fetch(PDO::FETCH_ASSOC);
+$stmt = $db->prepare("SELECT * FROM users WHERE id = ?");
+$result = $stmt->execute([$userId]);
+/*SQL文が実行に失敗した場合、ステータスコード500をレスポンス*/
+if (!$result) {
+    http_response_code(500);
+    exit;
+}
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-$stmt2 = $db->prepare("SELECT * FROM users_data WHERE user_id = ?");
-$stmt2->execute([$userId]);
-$row2 = $stmt2->fetch(PDO::FETCH_ASSOC);
+$stmt = $db->prepare("SELECT * FROM users_data WHERE user_id = ?");
+$result = $stmt->execute([$userId]);
+/*SQL文が実行に失敗した場合、ステータスコード500をレスポンス*/
+if (!$result) {
+    http_response_code(500);
+    exit;
+}
+$user_data = $stmt->fetch(PDO::FETCH_ASSOC);
 
 echo json_encode([
-    "user_id" => $row2["user_id"],
-    "username" => $row1["username"],
-    "owncoin" => $row2["owncoin"]
+    "user_id" => $user_data["user_id"],
+    "username" => $user["username"],
+    "owncoin" => $user_data["owncoin"]
 ]);
